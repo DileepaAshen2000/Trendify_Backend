20dcb0ac138ac81ae704774ebc36c59230714aec
Ashen <dileepaashen81@gmail.com>
2024-11-18 17:43:29 +0530

    add springboot project

---
+++ src/main/java/com/trendify/trendifyBackend/TrendifyBackendApplication.java
@@ -0,0 +1,14 @@
+package com.trendify.trendifyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendifyBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TrendifyBackendApplication.class, args);
+	}
+
+}
