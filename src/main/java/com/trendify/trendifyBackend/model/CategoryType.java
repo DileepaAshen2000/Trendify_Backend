fb37e6e7ac0f3f1e8defc0883239df7a211d560e
Siluni <wijesinghe.anj@gmail.com>
2024-11-23 20:34:39 +0530

    create entities

---
+++ src/main/java/com/trendify/trendifyBackend/model/CategoryType.java
@@ -0,0 +1,34 @@
+package com.trendify.trendifyBackend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "category_type")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryType {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",nullable = false)
+    @JsonIgnore
+    private Category category;
+}
--- src/main/java/com/trendify/trendifyBackend/model/Product.java
+++ src/main/java/com/trendify/trendifyBackend/model/Product.java
@@ -1,38 +1,80 @@
 package com.trendify.trendifyBackend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 
 @Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue
+    private UUID id;
 
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column
     private String description;
 
-    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private ProductCategory category;
-
-    @Column(nullable = false)
-    private String size; // e.g., "S", "M", "L", "XL"
+    private BigDecimal price;
 
     @Column(nullable = false)
-    private String color;
+    private String brand;
+
+    @Column
+    private Float rating;
 
     @Column(nullable = false)
-    private Double price;
+    private boolean isNewArrival;
+
+    @Column(nullable = false,unique = true)
+    private String slug;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date createdAt;
 
     @Column(nullable = false)
-    private Integer stock;
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date updatedAt;
 
-    // Getters and Setters
+    @OneToMany(mappedBy = "product",cascade = CascadeType.ALL)
+    private List<ProductVariant> productVariants;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id",nullable = false)
+    @JsonIgnore
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "categoryType_id",nullable = false)
+    @JsonIgnore
+    private CategoryType categoryType;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<Resources> resources;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = new java.util.Date();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = new java.util.Date();
+    }
 }
-
